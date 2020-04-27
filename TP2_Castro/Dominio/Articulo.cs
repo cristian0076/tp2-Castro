144b402d67399b8216ed7fa1a4bae2f727684de6
cristian <cristiangabrielcastro@gmail.com>
2020-04-27 17:46:17 -0300

    Segundo trabajo de programacion 3

---
+++ TP2_Castro/Dominio/Articulo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        private string Codigo;
+        private string Nombre;
+        private string Descripcion;
+        private Marca marca;
+        private Categoria categoria;
+        private string Imagen;
+        private decimal Precio;
+
+
+        public Categoria Categoria { get => categoria; set => categoria = value; }
+        public Marca Marca { get => marca; set => marca = value; }
+        public string codigo { get => Codigo; set => Codigo = value; }
+        public string descripcion { get => Descripcion; set => Descripcion = value; }
+
+
+        public string nombre { get => Nombre; set => Nombre = value; }
+        public string imagen { get => Imagen; set => Imagen = value; }
+        public decimal precio { get => Precio; set => Precio = value; }
+
+        public Articulo()
+        {
+
+        }
+
+
+
+        public override string ToString()
+        {
+            return Nombre;
+
+        }
+    }
+}
